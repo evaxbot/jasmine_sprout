7f49b03546b4cae4b450bd5e6df17bfd6abbb4fd
evaxbot <harshavarthan.0309@gmail.com>
2021-08-02 23:41:42 +0530

    adapt to caf

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,11 +15,11 @@
 # limitations under the License.
 #
 
-# inherit MI A2 device config for ShapeShiftOS
+# inherit MI A2 device config for P404
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/ssos_jasmine_sprout.mk
+    $(LOCAL_DIR)/p404_jasmine_sprout.mk
 
 COMMON_LUNCH_CHOICES := \
-    ssos_jasmine_sprout-user \
-    ssos_jasmine_sprout-userdebug \
-    ssos_jasmine_sprout-eng
+    p404_jasmine_sprout-user \
+    p404_jasmine_sprout-userdebug \
+    p404_jasmine_sprout-eng
--- p404_jasmine_sprout.mk
+++ p404_jasmine_sprout.mk
@@ -17,8 +17,13 @@
 # Inherit device configuration
 $(call inherit-product, device/xiaomi/jasmine_sprout/device.mk)
 
-# Inherit ShapeShiftOS product configuration
-$(call inherit-product, vendor/ssos/config/common_full_phone.mk)
+# Inherit some common Project-404 stuff
+$(call inherit-product, vendor/404/configs/common.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
+P404_BUILDTYPE := RIPPA
+
+$(call inherit-product, vendor/google/pixel/config.mk)
+$(call inherit-product, vendor/google/gms/config.mk)
 
 # Define first api level
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
@@ -40,20 +45,6 @@ PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_NAME := ssos_jasmine_sprout
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MODEL := Mi A2
-
-# Inherit ShapeShiftOS Official build stuff.
-SSOS_BUILD_TYPE := OFFICIAL
-TARGET_FACE_UNLOCK_SUPPORTED := true
-TARGET_BOOT_ANIMATION_RES := 1080
-
-PRODUCT_PRODUCT_PROPERTIES += \
-  ro.ssos.cpu=SDM660
-
-# Gapps
-IS_PHONE := true
-TARGET_GAPPS_ARCH := arm64
-TARGET_SUPPORTS_GOOGLE_RECORDER := true
-TARGET_INCLUDE_STOCK_ARCORE := true
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
